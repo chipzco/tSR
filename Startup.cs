396b5a45b6fad27d068b43715eee51cf085482d9
charles <charles.daniel@relovate.com>
2016-12-31 17:44:29 -0500

    Init Proj Files

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tSR.Startup))]
+namespace tSR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
